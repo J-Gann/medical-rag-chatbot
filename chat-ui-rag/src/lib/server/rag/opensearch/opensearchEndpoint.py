c39358a04bbb2eea739218e5dfdfea6bfe906786
Jonas Gann <jonas@gann.digital>
2024-02-29 17:27:45 +0100

    fix .env handling

--- chat-ui-rag/src/lib/server/rag/opensearch/opensearchEndpoint.py
+++ chat-ui-rag/src/lib/server/rag/opensearch/opensearchEndpoint.py
@@ -12,35 +12,6 @@ import json
 from opensearchpy import OpenSearch
 from transformers import AutoTokenizer, AutoModel, BertForSequenceClassification, AutoModelForQuestionAnswering
 
-env_file_path = Path("/home/paperspace/QA-INLPT-WS2023/chat-ui-rag/.env")
-
-data = [{ "name": "Biomistral RAG",
-      "chatPromptTemplate": "<s>{{#each messages}}{{#ifUser}}[INST] {{#if @first}}{{#if @root.preprompt}}{{@root.preprompt}}\n{{/if}}{{/if}} {{content}} [/INST]{{/ifUser}}{{#ifAssistant}}{{content}}</s> {{/ifAssistant}}{{/each}}",
-      "preprompt": "You are a medical assistant. Answer questions truthfully. Base your answer soly on the given information.",
-      "websiteUrl": "https://huggingface.co/BioMistral/BioMistral-7B",
-      "parameters": {
-        "temperature": 1,
-        "max_new_tokens": 1024,
-        "stop": ["</s>"]
-      },
-      "rag": {
-        "vectorStoreType": "opensearch",
-        "url": "http://127.0.0.1:9300"
-      },
-      "endpoints": [
-        {
-         "type": "ollama",
-         "url" : "http://127.0.0.1:11434",
-         "ollamaName" : "cniongolo/biomistral"
-        }
-      ]
-  }
-]
-  
-models = json.dumps(data)
-
-set_key(dotenv_path=env_file_path, key_to_set="MODELS", value_to_set=models)
-
 if torch.cuda.is_available():
   device = "cuda:0"
 else:
@@ -55,7 +26,7 @@ Add the whole path if pubmed_data is not detectet
 e.g. /home/paperspace/QA-INLPT-WS2023/chat-ui-rag/src/lib/server/rag/pinecone/pubmed_data
 """
 
-with open("/home/paperspace/QA-INLPT-WS2023/chat-ui-rag/src/lib/server/rag/pinecone/pubmed_data") as stream:
+with open("./pubmed_data") as stream:
     for article in Medline.parse(stream):
 
         if not "PMID" in article:
